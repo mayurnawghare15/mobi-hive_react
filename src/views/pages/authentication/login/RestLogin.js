4cabe7a0610fef5e111bf8b3d69674dcd56460ee
mayurnawghare15 <mayurnawghare57@outlook.com>
2023-07-12 18:28:54 +0530

    changed login api

--- src/views/pages/authentication/login/RestLogin.js
+++ src/views/pages/authentication/login/RestLogin.js
@@ -125,20 +125,13 @@ const RestLogin = (props, { ...others }) => {
                 onSubmit={(values, { setErrors, setStatus, setSubmitting }) => {
                     try {
                         axios
-                            .post('https://sandbox.credithive.co.uk/webservices/v1/token/auth/', {
+                            .post(BASE_URL + 'v2/token/auth/', {
                                 username: values.username,
                                 password: values.password
                             })
                             .then(function (response) {
-                                console.log(response.data, 'Data');
-                                console.log(response.data.status, 'Success');
-                                console.log(response.data.data.token);
-                                if (response.data.status === 200) {
+                                if (response.status) {
                                     toast.success('Login Sucessfully');
-
-                                    console.log(response.data);
-                                    console.log(response.data.data.token);
-                                    console.log(response.data.user);
                                     dispatcher({
                                         type: ACCOUNT_INITIALIZE,
                                         payload: {
@@ -153,9 +146,8 @@ const RestLogin = (props, { ...others }) => {
                                     }
                                 } else {
                                     toast.error('Please Enter Correct Credentials');
-
                                     setStatus({ success: false });
-                                    setErrors({ submit: response.data.message });
+                                    setErrors({ submit: response.message });
                                     setSubmitting(false);
                                 }
                             })
